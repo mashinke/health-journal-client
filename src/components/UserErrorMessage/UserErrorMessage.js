e7e9fcdf5916d5e38868cb294a9ce9a56d091a38
Masha Trius <triusmaria@gmail.com>
2021-01-26 23:14:04 -0600

    write test for UserErrorMessage

--- src/components/UserErrorMessage/UserErrorMessage.js
+++ src/components/UserErrorMessage/UserErrorMessage.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import UserContext from '../../contexts/UserContext';
 
-export default function UserErrorMessage() {
+function UserErrorMessage() {
   const userContext = useContext(UserContext);
   if (userContext.error) {
     return <div>There was an error with your login. Please try again.</div>;
   }
   return null;
 }
+
+export default UserErrorMessage;
