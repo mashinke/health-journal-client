80f6923562ca4c6c4f3d5a885778cca8c9e1b409
Masha Trius <triusmaria@gmail.com>
2021-01-26 22:59:54 -0600

    write test for SelectMultipleComponents

---
+++ src/components/SelectMultipleComponents/SelectMultipleComponents.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import ThemeService from '../../services/theme-service';
+import * as components from './SelectMultipleComponents';
+
+Object.entries(components).forEach(([componentName, Component]) => {
+  it(`${componentName} renders without crashing`,
+    () => {
+      const div = document.createElement('div');
+      ReactDOM.render((
+        <ThemeProvider theme={(ThemeService.getTheme())}>
+          <Component />
+        </ThemeProvider>
+      ),
+      div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+});
