e74bf587a1f568f15d5d460a3e9da7e3194e3661
Masha Trius <triusmaria@gmail.com>
2021-01-26 21:29:16 -0600

    write smoke test for DashboardLoading component

---
+++ src/components/DashboardLoading/DashboardLoading.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import ThemeService from '../../services/theme-service';
+import DashboardLoading from './DashboardLoading';
+
+it('renders without crashing',
+  () => {
+    const div = document.createElement('div');
+    ReactDOM.render((
+      <ThemeProvider theme={(ThemeService.getTheme())}>
+        <DashboardLoading />
+      </ThemeProvider>
+    ),
+    div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
